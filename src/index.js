f4491a3c1d30786b7a3e8b7f15a316ba94ad82dd
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-01-22 22:46:25 +0700

    Install font Roboto Slab

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-import 'normalize.css';
-import 'typeface-roboto';
+import "normalize.css";
+import "typeface-roboto";
+import "typeface-roboto-slab";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
