a2d23476477ad54e7c778de9a6b8b2de0971d6dc
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-02-06 21:56:38 +0700

    Update listNote helper to use '_id' instead of 'id'

--- src/__tests__/helper/listNote.test.js
+++ src/__tests__/helper/listNote.test.js
@@ -5,17 +5,17 @@ let checklist;
 beforeEach(() => {
   checklist = [
     {
-      id: 1,
+      _id: "1",
       label: "Learn React",
       isChecked: false
     },
     {
-      id: 2,
+      _id: "2",
       label: "Learn Redux",
       isChecked: false
     },
     {
-      id: 3,
+      _id: "3",
       label: "Learn Redux Saga",
       isChecked: false
     }
@@ -23,13 +23,13 @@ beforeEach(() => {
 });
 
 test("checkListItem()", () => {
-  let result = checkListItem(checklist, 1);
+  let result = checkListItem(checklist, "1");
   expect(result[0].isChecked).toEqual(true);
-  result = checkListItem(result, 1);
+  result = checkListItem(result, "1");
   expect(result[0].isChecked).toEqual(false);
 });
 
 test("setListItemLabel()", () => {
-  const result = setListItemLabel(checklist, "Learn SVG", 1);
+  const result = setListItemLabel(checklist, "Learn SVG", "1");
   expect(result[0].label).toEqual("Learn SVG");
 });
